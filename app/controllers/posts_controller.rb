ba2f619e915054f0e406fb3abd8884e7ae87f2cd
Gabriel Kay <gabrielkay1@gmail.com>
2019-06-05 10:57:14 -0400

    indents to spaces

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,27 +1,27 @@
 class PostsController < ApplicationController
 
-	def index
-		@posts = Post.all
-	end
+  def index
+    @posts = Post.all
+  end
 
-	def show
-		redirect_to Post.find(params[:id]).link
-	end
+  def show
+    redirect_to Post.find(params[:id]).link
+  end
 
-	def new
-		@post = Post.new
-	end
+  def new
+    @post = Post.new
+  end
 
-	def create
-		@post = Post.new(post_params)
-		if @post.save
-			redirect_to posts_path
-		else
-			render 'new'
-		end
-	end
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
 
-	private def post_params
-		params.require(:post).permit(:title, :link)
-	end
+  private def post_params
+    params.require(:post).permit(:title, :link)
+  end
 end
