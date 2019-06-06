de85b4c662136f7f3b47bfc21d18779736e97d0e
Gabriel Kay <gabrielkay1@gmail.com>
2019-06-06 09:31:53 -0400

    validates title is not empty

--- app/validators/post_validator.rb
+++ app/validators/post_validator.rb
@@ -7,6 +7,7 @@ class PostValidator < ActiveModel::Validator
   end
 
   def validate(record)
+    record.errors[:base] << 'You must include a title for your post' if record.title.to_s.strip.empty?
     record.errors[:base] << 'You must submit a valid URL. You entered: ' + record.link unless valid_url?(record.link)
   end
 end
