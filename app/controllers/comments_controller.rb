7bda4635d453b5959a2fd4a9e0aefa2046fa1054
Gabriel Kay <gabrielkay1@gmail.com>
2019-06-14 09:15:42 -0400

    removed autogenerated spec files and fixed typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,28 +1,10 @@
 class CommentsController < ApplicationController
-  # # GET /comments
-  # # GET /comments.json
-  # def index
-  #   @comments = Comment.all
-  # end
 
-  # # GET /comments/1
-  # # GET /comments/1.json
   def show
     @comment = Comment.find(params[:id])
     @new_comment = Comment.new
   end
 
-  # # GET /comments/new
-  # def new
-  #   @comment = Comment.new
-  # end
-
-  # # GET /comments/1/edit
-  # def edit
-  # end
-
-  # POST /comments
-  # POST /comments.json
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
--- db/migrate/20190612134117_create_comments.rb
+++ db/migrate/20190612134117_create_comments.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
